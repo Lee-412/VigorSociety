83a6f981e45af314a10effe47ac62fb41d6433ff
LeHien2818 <hienltv281823@gmail.com>
2024-03-27 23:03:30 +0700

    done progress

---
+++ src/TrackingComponents/Tracking_Nutrion.tsx
@@ -0,0 +1,25 @@
+import './Tracking.css'
+import React from 'react';
+import { Flex, Progress } from 'antd';
+
+const TrackingComponent: React.FC = () => (
+   <div className='tracking'>
+    <Flex gap="small" wrap="wrap" className='progress-container'>
+        <div className='p1'>
+            <Progress type="circle" percent={75} size={90} strokeColor={'#42f5f2'} />
+            <p>Progress</p>
+        </div> 
+        <div className='p2'>
+            <Progress type="circle" percent={70} status="exception" size={90} gapDegree={36}/>
+            <p>Long term goal</p>
+        </div>
+        <div className='p3'>
+            <Progress type="circle" percent={100} size={90} />
+            <p>Today goal</p>
+        </div>
+    </Flex>
+   </div> 
+  
+);
+
+export default TrackingComponent;
